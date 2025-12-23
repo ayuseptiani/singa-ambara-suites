eb1b78d5bb85855f67eec53ff40f88c236973311
Ryandika <igederyandikapramudiawardana79@gmail.com>
2025-12-23 17:22:59 +0800

    Menghubungkan ke backend, ganti next_public_api_url menjadi web api domainesia

--- lib/axios.ts
+++ lib/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     // Ganti dengan URL Laravel Anda
-    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000/api',
+    baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://api.singa-ambara-suites.web.id/api',
     
     headers: {
         'Content-Type': 'application/json',
